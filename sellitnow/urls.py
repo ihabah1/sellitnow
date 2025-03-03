63a193c0f56a25ca75b56d1bc5d2dc72167b076a
ihabah1 <ihab.ah1@gmail.com>
2025-03-03 06:06:55 +0200

    ..

---
+++ sellitnow/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from app.views import HomeView, LobbyView, ProductCreateView, AdminDashboardView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='index'),  # Default home page
+    path('admin/', admin.site.urls),  # Django admin panel
+    path('lobby/', LobbyView.as_view(), name='lobby'),  # Lobby view for products
+    path('add-product/', ProductCreateView.as_view(), name='add_product'),  # Product creation page
+    path('admin-dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),  # Admin dashboard
+    path('accounts/', include('allauth.urls')),  # Authentication with Django Allauth
+]
